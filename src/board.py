2da678582969ba74b2aae57d848b0c14d3b57106
DuyThai38 <dthai3816@gmail.com>
2025-04-21 13:25:21 +0700

    hurry up

--- src/board.py
+++ src/board.py
@@ -100,7 +100,7 @@ class Board:
     piece.clear_move()
     self.last_move = move
 
-  
+
 
 
   """===================== KIỂM TRA NƯỚC ĐI HỢP LỆ ======================="""
@@ -134,22 +134,33 @@ class Board:
   def in_check(self, piece, move):
     temp_piece = copy.deepcopy(piece)
     temp_board = copy.deepcopy(self)
-    temp_board.move(temp_piece, move, testing = True)
+    temp_board.move(temp_piece, move, testing=True)
 
+    # Tìm vị trí vua sau nước đi
+    king_pos = None
+    for r in range(ROWS):
+        for c in range(COLS):
+            p = temp_board.squares[r][c].piece
+            if isinstance(p, King) and p.color == piece.color:
+                king_pos = (r, c)
+                break
+        if king_pos:
+            break
+
+    # Kiểm tra xem quân địch có thể tấn công vị trí vua không
     for row in range(ROWS):
-      for col in range(COLS):
-        if temp_board.squares[row][col].has_enemy_piece(piece.color):
-          p = temp_board.squares[row][col].piece
-          temp_board.calc_moves(p, row, col, bool = False)
-          for m in p.moves:
-            if isinstance(m.final.piece, King):
-              return True
+        for col in range(COLS):
+            if temp_board.squares[row][col].has_enemy_piece(piece.color):
+                enemy_piece = temp_board.squares[row][col].piece
+                temp_board.calc_moves(enemy_piece, row, col, bool=False)
+                for m in enemy_piece.moves:
+                    if (m.final.row, m.final.col) == king_pos:
+                        return True
     return False
 
 
   """============ TÍNH TOÁN CÁC NƯỚC ĐI HỢP LỆ CẢU QUÂN CỜ =============== """
   def calc_moves(self, piece, row, col, bool = True):
-    
     def pawn_moves():
       steps = 1 if piece.moved else 2
       # vertical moves
@@ -166,10 +177,8 @@ class Board:
             # check potencial checks
             if bool:
               if not self.in_check(piece, move):
-                #append new move
                 piece.add_move(move)
             else:
-              #append new move
               piece.add_move(move)
           else: break
         else: break
@@ -318,109 +327,63 @@ class Board:
           possible_move_col = possible_move_col + col_incr
 
     def king_moves():
+      # Các ô xung quanh vua
       adjs = [
-        (row - 1, col + 0),
-        (row - 1, col + 1),
-        (row + 0, col + 1),
-        (row + 1, col + 1),
-        (row + 1, col + 0),
-        (row + 1, col - 1),
-        (row + 0, col - 1),
-        (row - 1, col - 1)
+          (row - 1, col), (row - 1, col + 1),
+          (row,     col + 1), (row + 1, col + 1),
+          (row + 1, col), (row + 1, col - 1),
+          (row,     col - 1), (row - 1, col - 1)
       ]
 
-      # normal moves
-      for possible_move in adjs:
-        possible_move_row, possible_move_col = possible_move
-        if Square.in_range(possible_move_row, possible_move_col):
-          if self.squares[possible_move_row][possible_move_col].isempty_or_enemy(piece.color):
-            #create squares of new move
-            initial = Square(row, col)
-            final = Square(possible_move_row, possible_move_col) # piece = piece
-            # create a new move
-            move = Move(initial, final)
-            # check potencial checks
-            if bool:
-              if not self.in_check(piece, move):
-                #append new move
-                piece.add_move(move)
-              else: break
-            else:
-              #append new move
-              piece.add_move(move)
+      # Các nước đi thông thường
+      for r, c in adjs:
+          if Square.in_range(r, c):
+              if self.squares[r][c].isempty_or_enemy(piece.color):
+                  initial = Square(row, col)
+                  final = Square(r, c)
+                  move = Move(initial, final)
 
-      # castling moves
+                  if bool:
+                      if not self.in_check(piece, move):
+                          piece.add_move(move)
+                  else:
+                      piece.add_move(move)
+
+      # Kiểm tra castling nếu vua chưa di chuyển
       if not piece.moved:
-        # queen castling
-        left_rook = self.squares[row][0].piece
-        if isinstance(left_rook, Rook):
-          if not left_rook.moved:
-            for c in range(1, 4):
-              # castling is not possible (has pieces betwwen)
-              if self.squares[row][c].has_piece(): 
-                break
-              if c == 3:
-                # add left rook to king 
-                piece.left_rook = left_rook
-                # rook move
-                initial = Square(row, 0)
-                final = Square(row, 3)
-                moveR = Move(initial, final)
-               
-                # king move
-                initial = Square(row, col)
-                final = Square(row, 2)
-                moveK = Move(initial, final)
-            
-                # check potencial checks
-                if bool:
-                  if not self.in_check(piece, moveK) and not self.in_check(left_rook, moveR):
-                    # append new move to rook
-                    left_rook.add_move(moveR)
-                    # append new move to king
-                    piece.add_move(moveK)
-                  else: break
-                else:
-                  # append new move to rook
-                  left_rook.add_move(moveR)
-                  # append new move to king
-                  piece.add_move(moveK)
+          # Queen-side castling (bên trái)
+          left_rook = self.squares[row][0].piece
+          if isinstance(left_rook, Rook) and not left_rook.moved:
+              if all(not self.squares[row][c].has_piece() for c in range(1, 4)):
+                  moveK = Move(Square(row, col), Square(row, 2))
+                  moveR = Move(Square(row, 0), Square(row, 3))
 
-        # king castling
-        right_rook = self.squares[row][7].piece
-        if isinstance(right_rook, Rook):
-          if not right_rook.moved:
-            for c in range(5, 7):
-              # castling is not possible (has pieces betwwen)
-              if self.squares[row][c].has_piece(): 
-                break
-              if c == 6:
-                # add right rook to king 
-                piece.right_rook = right_rook
-                # rook move
-                initial = Square(row, 7)
-                final = Square(row, 5)
-                moveR = Move(initial, final)
+                  if bool:
+                      if not self.in_check(piece, moveK) and not self.in_check(left_rook, moveR):
+                          piece.left_rook = left_rook
+                          piece.add_move(moveK)
+                          left_rook.add_move(moveR)
+                  else:
+                      piece.left_rook = left_rook
+                      piece.add_move(moveK)
+                      left_rook.add_move(moveR)
 
-                # king move
-                initial = Square(row, col)
-                final = Square(row, 6)
-                moveK = Move(initial, final)
+          # King-side castling (bên phải)
+          right_rook = self.squares[row][7].piece
+          if isinstance(right_rook, Rook) and not right_rook.moved:
+              if all(not self.squares[row][c].has_piece() for c in range(5, 7)):
+                  moveK = Move(Square(row, col), Square(row, 6))
+                  moveR = Move(Square(row, 7), Square(row, 5))
 
-
-                # check potencial checks
-                if bool:
-                  if not self.in_check(piece, moveK) and not self.in_check(right_rook, moveR):
-                    # append new move to rook
-                    right_rook.add_move(moveR)
-                    # append new move to king
-                    piece.add_move(moveK)
-                  else: break
-                else:
-                  # append new move to rook
-                  right_rook.add_move(moveR)
-                  # append new move to king
-                  piece.add_move(moveK)
+                  if bool:
+                      if not self.in_check(piece, moveK) and not self.in_check(right_rook, moveR):
+                          piece.right_rook = right_rook
+                          piece.add_move(moveK)
+                          right_rook.add_move(moveR)
+                  else:
+                      piece.right_rook = right_rook
+                      piece.add_move(moveK)
+                      right_rook.add_move(moveR)
 
     if isinstance(piece ,Pawn): 
       pawn_moves()
